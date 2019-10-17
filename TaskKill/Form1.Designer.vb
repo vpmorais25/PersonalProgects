a953511c11f745ea27fd3990080d5db83ce36602
vpmorais25 <47580635+vpmorais25@users.noreply.github.com>
2019-10-16 23:30:32 -0300

    Add files via upload

---
+++ TaskKill/Form1.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TBprocesso = New System.Windows.Forms.TextBox()
+        Me.Bkill = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TBprocesso
+        '
+        Me.TBprocesso.Location = New System.Drawing.Point(13, 13)
+        Me.TBprocesso.Name = "TBprocesso"
+        Me.TBprocesso.Size = New System.Drawing.Size(229, 20)
+        Me.TBprocesso.TabIndex = 0
+        '
+        'Bkill
+        '
+        Me.Bkill.Location = New System.Drawing.Point(13, 58)
+        Me.Bkill.Name = "Bkill"
+        Me.Bkill.Size = New System.Drawing.Size(75, 23)
+        Me.Bkill.TabIndex = 1
+        Me.Bkill.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(254, 97)
+        Me.Controls.Add(Me.Bkill)
+        Me.Controls.Add(Me.TBprocesso)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(270, 136)
+        Me.MinimumSize = New System.Drawing.Size(270, 136)
+        Me.Name = "Form1"
+        Me.Opacity = 0.9R
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "TASK  KILL"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TBprocesso As TextBox
+    Friend WithEvents Bkill As Button
+End Class
---
+++ TaskKill/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associada a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("TaskKill")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("TaskKill")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("f223aa88-6fa6-4c0a-8160-87d590f80e7b")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+' utilizando o "*" como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
